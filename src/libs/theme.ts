9cfa1b61aed4db7b2f03b6c51cba7e0b9ff8ef9b
ikeryo <sruyrof@icloud.com>
2022-09-11 15:08:43 +0900

    made toggleThemeButton

--- src/libs/theme.ts
+++ src/libs/theme.ts
@@ -1,26 +1,11 @@
-import React, { useContext, createContext } from "react";
-import { atom, useRecoilState, useSetRecoilState } from 'recoil'
+import { atom, useSetRecoilState } from 'recoil'
 
 export type Theme = 'light' | 'dark'
 
-const themeState = atom<Theme>({
+export const themeState = atom<Theme>({
   key: 'themeState',
   default: 'light'
 })
 
 export const useSetTheme = () => useSetRecoilState(themeState)
 
-
-export const useTheme = () => {
-  const [theme, setTheme] = useRecoilState(themeState)
-
-  const toggleTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light'
-    setTheme(newTheme)
-    window.localStorage.setItem('theme', newTheme)
-    const root = window.document.documentElement
-    root.setAttribute('data-theme', newTheme)
-  }
-
-  return { theme, toggleTheme }
-}
