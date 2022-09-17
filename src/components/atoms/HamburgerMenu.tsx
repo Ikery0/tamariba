49a37ef020f4ca79f3caf2ed6fe87f1b7fa17c31
ikeryo <sruyrof@icloud.com>
2022-09-17 13:37:57 +0900

    added area-expanded

--- src/components/atoms/HamburgerMenu.tsx
+++ src/components/atoms/HamburgerMenu.tsx
@@ -3,19 +3,28 @@ import { useState } from "react";
 
 type Text = 'menu' | 'close'
 
-const HamburgerMenu = () => {
+const HamburgerMenu: NextPage = () => {
   const [text, setText] = useState<Text>('menu')
+  const [expanded, setExpanded] = useState<boolean>(false)
 
-  const toggleText = (): void => setText(text === 'menu' ? 'close' : 'menu')
+  const toggleMenu = (): void =>  {
+    setText(text === 'menu' ? 'close' : 'menu')
+    setExpanded(!expanded)
+    
+  }
 
   return (
     <>
       <button
-        className="fixed grid place-items-center w-14 h-14 bottom-4 left-1/2 -translate-x-1/2 m-auto bg-gray-700 rounded-full lg:hidden"
-        onClick={toggleText}
+        className="fixed grid place-items-center w-14 h-14 bottom-4 left-1/2 -translate-x-1/2 m-auto bg-gray-700 rounded-full z-20 lg:hidden"
+        onClick={toggleMenu}
+        aria-expanded={expanded}
       >
         <span className="uppercase text-xs font-bold pl-px text-gray-50 text-opacity-90">{text}</span>
       </button>
+      <div className={`bg-gray-700 z-10`}>
+        sp menu
+      </div>
     </>
   )
 }
