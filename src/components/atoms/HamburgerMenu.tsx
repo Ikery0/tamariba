34d1177c709eb83345fedc3ca29460c457284a93
ikeryo <sruyrof@icloud.com>
2022-09-18 09:43:23 +0900

    refactoring toggleHamburger function

--- src/components/atoms/HamburgerMenu.tsx
+++ src/components/atoms/HamburgerMenu.tsx
@@ -1,29 +1,30 @@
 import { NextPage } from "next";
 import { useEffect, useState } from "react";
 
-type Text = 'menu' | 'close'
-type SpMenuClass = string[]
+type Obj = {
+  text: 'menu' | 'close',
+  ariaHidden: 'true' | 'false'
+}
 
-const openClasses: SpMenuClass = ['opacity-100', 'visible']
-const closeClasses: SpMenuClass = ['opacity-0', 'invisible']
+const closedObj: Obj = {
+  text: 'menu',
+  ariaHidden: 'true'
+}
+
+const openedObj: Obj = {
+  text: 'close',
+  ariaHidden: 'false'
+}
 
 const HamburgerMenu: NextPage = () => {
-  const [text, setText] = useState<Text>('menu')
-  const [expanded, setExpanded] = useState<boolean>(false)
-  const [hidden, setHidden] = useState<boolean>(true)
+  const [isOpen, setIsOpen] = useState<boolean>(false)
 
-  let spMenuClasses = closeClasses
+  let obj: Obj = closedObj
 
   const toggleHamburger = (): void => {
-    setText(text === 'menu' ? 'close' : 'menu')
-    setExpanded(!expanded)
-    setHidden(!hidden)
-
-    spMenuClasses = closeClasses ? closeClasses : openClasses
+    setIsOpen(!isOpen)
+    obj = isOpen ? openedObj : closedObj
   }
-
-
-
 
   return (
     <>
@@ -31,24 +32,24 @@ const HamburgerMenu: NextPage = () => {
         className="fixed grid place-items-center w-14 h-14 bottom-4 left-1/2 -translate-x-1/2 m-auto bg-gray-700 rounded-full lg:hidden"
         onClick={toggleHamburger}
         aria-controls="spMenu"
-        aria-expanded={expanded}
+        aria-expanded={isOpen}
       >
-        <span className="uppercase text-xs font-bold pl-px text-gray-50 text-opacity-90">{text}</span>
+        <span className="uppercase text-xs font-bold pl-px text-gray-50 text-opacity-90">{obj.text}</span>
       </button>
       {
-        expanded ? (
+        isOpen ? (
           <div
             id="spMenu"
-            className={`visible bg-gray-700 z-10 opacity-100 duration-300`}
-            aria-hidden={hidden}
+            className={`visible bg-gray-200 z-10 opacity-100 duration-300`}
+            aria-hidden={obj.ariaHidden}
           >
             sp menu
           </div>
         ) : (
           <div
             id="spMenu"
-            className={`invisible bg-gray-700 z-10 opacity-0 duration-300`}
-            aria-hidden={hidden}
+            className={`invisible bg-gray-200 z-10 opacity-0 duration-300`}
+            aria-hidden={obj.ariaHidden}
           >
             sp menu
           </div>
