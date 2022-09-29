e619da7d17cf009bf30362b9cebdc61669055b18
ikeryo <sruyrof@icloud.com>
2022-09-29 16:16:34 +0900

    add margin top to footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 const Footer: NextPage = () => {
 
   return (
-    <footer className="py-10">
+    <footer className="py-10 mt-10 lg:mt-20">
       <small className="block text-center">&copy; 2022 ikeryo All Rights Reserved.</small>
     </footer>
   )
